5d48c140bf4318e795e5c388bd0e3ba986cc1f73
Shreya Hegde <shreyahegde294@gmail.com>
2022-05-13 17:44:16 +0530

    Added Admin Login Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import Contact from './components/Footer';
+import VoterLogin from './components/VoterLogin';
+import AdminLogin from './components/AdminLogin';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />}/>
+        <Route path="/contact" exact element={<Contact />}/>
+        <Route path="/sign-in" exact element={<VoterLogin />}/>
+        <Route path="/admin" exact element={<AdminLogin />}/>
       </Routes>
     </Router>
     </>
