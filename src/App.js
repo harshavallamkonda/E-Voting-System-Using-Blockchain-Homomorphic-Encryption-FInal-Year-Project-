2338364973203ce7d9e666e76428db6a528cdc5a
Shreya Hegde <shreyahegde294@gmail.com>
2022-05-18 20:26:04 +0530

    changed files

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 import Contact from './components/Footer';
-import VoterLogin from './components/VoterLogin';
-import AdminLogin from './components/AdminLogin';
+import VoterLogin from './components/pages/VoterLogin';
+import AdminLogin from './components/pages/AdminLogin';
+import Voting from './components/pages/Voting';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" exact element={<Home />}/>
         <Route path="/contact" exact element={<Contact />}/>
         <Route path="/sign-in" exact element={<VoterLogin />}/>
+        <Route path='/voting' exact element={<Voting />} />
         <Route path="/admin" exact element={<AdminLogin />}/>
       </Routes>
     </Router>
