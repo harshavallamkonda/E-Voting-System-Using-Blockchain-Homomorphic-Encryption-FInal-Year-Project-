9c9bfc944fe9bd141f00ff7738d64880addce33a
PrateekGiridhar <prateek.giridhar2000@gmail.com>
2022-05-24 18:56:20 +0530

    made a brief of the voting page

--- src/components/pages/Voting.js
+++ src/components/pages/Voting.js
@@ -15,65 +15,153 @@ function Voting() {
     const [candidateID, setCandidateID] = useState(0)
     const [voterIndex, setvoterIndex] = useState(0)
 
-    const castVote = async (event, _candidateID) => {
+    const castVote = (event, _candidateID) => {
+
         event.preventDefault()
         setvoterIndex((voterIndex) => voterIndex + 1)
         setCandidateID(_candidateID)
         (candidateID === 1) ? setParty('REP') : 
         (candidateID === 2) ? setParty('DEM') :
-        setParty('KW')
+        (candidateID === 3) ? setParty('KW') :
+        alert(
+            "There is an error"
+        )
 
         window.confirm(
             "Press 'OK' to vote for Candidate" + {candidateID} + "of" + {party} + "?"
             )
 
-        await vote(voterIndex, candidateID)
-        
-        navigate('/')
+        try{
+            vote(voterIndex, candidateID)
+            alert(
+                "Your vote has successfully been cast"
+            )
+            navigate(`/`)
+        }catch(error){
+            alert(
+                " There was an error " + {error}
+            )
+        }
 
-        
     }
 
     return (
     
     <Container>
         <Row>
-            <Col className='justify-content-center d-flex'>
-            <Container>
-                <Row style={{marginTop: '5vh', backgroundColor: '#ffff00'}}>
-                    <div 
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'left',
-                        padding: '3vw'
-                    }}>
-                        <img 
-                        style={{ height: '20vw',width: '30vw'}}
-                        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.americanbazaaronline.com%2Fwp-content%2Fuploads%2F2020%2F11%2FJoe-Biden.jpg&f=1&nofb=1"
-                        alt="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbsmedia.business-standard.com%2F_media%2Fbs%2Fimg%2Fmisc%2F2020-07%2F18%2Ffull%2F20200701146L.jpg&f=1&nofb=1"
-                        onClick={ castVote(1) }
-                        />
-                    </div>
-                </Row>
-                <Row 
-                className='justify-content-center d-flex'
-                style={{ marginTop: '5vh'}}
-                >
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        fontSize: '8vw',
-                        padding: '10px',
-                        backgroundColor: '#ffff00'
-                    }}>
-                        3
-                    </div>
-                </Row>
-            </Container>
-            </Col>
+        <Col className='justify-content-center d-flex'>
+          <Container>
+            <Row style={{ marginTop: "5vh", backgroundColor: "#ff0000" }}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  padding: "3vw"
+                }}
+              >
+                <img
+                  style={{
+                    height: "35vh",
+                    width: "20vw",
+                  }}
+                  src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.americanbazaaronline.com%2Fwp-content%2Fuploads%2F2020%2F11%2FJoe-Biden.jpg&f=1&nofb=1"
+                  alt="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbsmedia.business-standard.com%2F_media%2Fbs%2Fimg%2Fmisc%2F2020-07%2F18%2Ffull%2F20200701146L.jpg&f=1&nofb=1"
+                        
+                />
+              </div>
+            </Row>
+            <Row
+              style={{ marginTop: "5vh" }}
+              className='justify-content-center d-flex'
+            >
+              <Button 
+              style={{
+                  backgroundColor: '#ff00ff'
+              }}
+              onClick={(e) => {
+                  castVote(e, 1)
+              }}
+              >
+                Vote
+              </Button>
+            </Row>
+          </Container>
+        </Col>
+        <Col className='justify-content-center d-flex align-items-center'>
+        <Row style={{ marginTop: "5vh", backgroundColor: "#ff0000" }}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  padding: "3vw"
+                }}
+              >
+                <img
+                  style={{
+                    height: "35vh",
+                    width: "20vw",
+                  }}
+                  src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.americanbazaaronline.com%2Fwp-content%2Fuploads%2F2020%2F11%2FJoe-Biden.jpg&f=1&nofb=1"
+                  alt="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbsmedia.business-standard.com%2F_media%2Fbs%2Fimg%2Fmisc%2F2020-07%2F18%2Ffull%2F20200701146L.jpg&f=1&nofb=1"
+                        
+                />
+              </div>
+            </Row>
+            <Row
+              style={{ marginTop: "5vh" }}
+              className='justify-content-center d-flex'
+            >
+              <Button style={{
+                backgroundColor: '#ff00ff'
+                }}
+                onClick={(e) => {
+                    castVote(e, 2)
+                }}>
+                Vote
+              </Button>
+            </Row>
+        </Col>
+        <Col className='justify-content-center d-flex'>
+          <Container>
+            <Row style={{ marginTop: "5vh", backgroundColor: "#ff0000" }}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  padding: "3vw",
+                }}
+              >
+                <img
+                  style={{
+                    height: "35vh",
+                    width: "20vw",
+                  }}
+                  src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.americanbazaaronline.com%2Fwp-content%2Fuploads%2F2020%2F11%2FJoe-Biden.jpg&f=1&nofb=1"
+                  alt="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbsmedia.business-standard.com%2F_media%2Fbs%2Fimg%2Fmisc%2F2020-07%2F18%2Ffull%2F20200701146L.jpg&f=1&nofb=1"
+                        
+                />
+              </div>
+            </Row>
+            <Row
+              style={{ marginTop: "5vh" }}
+              className='justify-content-center d-flex'
+            >
+              <Button style={{
+                  backgroundColor: '#ff00ff'
+                }}
+                onClick={(e) => {
+                    castVote(e, 3)
+                }}>
+                Vote
+              </Button>
+            </Row>
+          </Container>
+        </Col>
         </Row>
     </Container>
     )
 }
+
+/*  */
 
 export default Voting;
