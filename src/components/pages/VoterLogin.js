f706f284f541539e5b167ba74c166ce12800b048
Shreya Hegde <shreyahegde294@gmail.com>
2022-07-07 16:22:23 +0530

    Formatting

--- src/components/pages/VoterLogin.js
+++ src/components/pages/VoterLogin.js
@@ -1,91 +1,94 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { connectDefault, loadVoterAccount } from '../web3/Web3'
-import { auth } from '../../firebase/config'
-import { RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { connectDefault, loadVoterAccount } from "../web3/Web3";
+import { auth } from "../../firebase/config";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 /* For routing to voting page after successfull verification */
-import { fetchVoterLogin } from '../pages/Voting'
-import { useNavigate } from 'react-router-dom';
+import { fetchVoterLogin } from "../pages/Voting";
+import { useNavigate } from "react-router-dom";
 
 function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="/">
-        VOTE
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+	return (
+		<Typography
+			variant='body2'
+			color='text.secondary'
+			align='center'
+			{...props}>
+			{"Copyright © "}
+			<Link color='inherit' href='/'>
+				VOTE
+			</Link>{" "}
+			{new Date().getFullYear()}
+			{"."}
+		</Typography>
+	);
 }
 
 const theme = createTheme();
 
 export default function VoterLogin() {
-  const [voterName, setvoterName] = React.useState("")
-  const [voterID, setvoterID] = React.useState("")
-  const [phoneNumber, setphoneNumber] = React.useState("")
-  const [OTP, setOTP] = React.useState("")
-  const [user, setUser] = React.useState([])
-  const [show, setshow] = React.useState(false)
-  const [collectionID, setcollectionID] = React.useState(0)
+	const [voterName, setvoterName] = React.useState("");
+	const [voterID, setvoterID] = React.useState("");
+	const [phoneNumber, setphoneNumber] = React.useState("");
+	const [OTP, setOTP] = React.useState("");
+	const [user, setUser] = React.useState([]);
+	const [show, setshow] = React.useState(false);
+	const [collectionID, setcollectionID] = React.useState(0);
 
-  /* function for navigation */
-  let navigate = useNavigate();
-  const routeChange = () => {
-    let path = `/voting`
-    navigate(path);
-  }
+	/* function for navigation */
+	let navigate = useNavigate();
+	const routeChange = () => {
+		let path = `/voting`;
+		navigate(path);
+	};
 
-  React.useEffect(() => {
-    connectDefault()
-  },[])
+	React.useEffect(() => {
+		connectDefault();
+	}, []);
 
-  /* For sending OTP after verifyng */
-  const sendOTP = (event) => {
+	/* For sending OTP after verifyng */
+	const sendOTP = (event) => {
+		if (voterName === "") {
+			return;
+		}
+		if (voterID === "") {
+			return;
+		}
+		if (phoneNumber === "" || phoneNumber.length < 13) {
+			return;
+		}
 
-      if (voterName === "") {
-        return;
-      }
-      if (voterID === ""){
-        return;
-      }
-      if (phoneNumber === "" || phoneNumber.length < 13) {
-        return;
-      }
-      
-      if (voterID === 'HTU6548521'){
-        setcollectionID(1)
-      }else if (voterID === 'KLE9852145'){
-        setcollectionID(2)
-      }else if (voterID === 'SEW6521432'){
-        setcollectionID(3)
-      }else {
-        setcollectionID(4)
-      }
+		if (voterID === "HTU6548521") {
+			setcollectionID(1);
+		} else if (voterID === "KLE9852145") {
+			setcollectionID(2);
+		} else if (voterID === "SEW6521432") {
+			setcollectionID(3);
+		} else {
+			setcollectionID(4);
+		}
 
-      
-      event.preventDefault()
-          window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
-            'size': 'invisible',
-            'callback': (response) => {
+		event.preventDefault();
+		window.recaptchaVerifier = new RecaptchaVerifier(
+			"recaptcha-container",
+			{
+				size: "invisible",
+				callback: (response) => {},
+			},
+			auth,
+		);
+		const appVerifier = window.recaptchaVerifier;
 
-            }
-          }, auth);
-          const appVerifier = window.recaptchaVerifier
-
-          
-          /*Query to check if voter's details exist in the election commission's database
+		/*Query to check if voter's details exist in the election commission's database
 
           const voterRef = collection(db, 'voter-details')
 
@@ -99,151 +102,152 @@ export default function VoterLogin() {
 
           //end of the query to see if voter exists
           */
-          
-          signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-          .then( confirmationResult => {
-              alert("OTP Sent Successfully!");
-              window.confirmationResult = confirmationResult
-              setshow(true)
-          })
-          .catch((err) => {
-              alert(err)
-              window.location.reload()
-          });
-  }
 
-  const handleSubmit = (event) => {
-    if (OTP === ""){
-      return;
-    }
-    event.preventDefault()
+		signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+			.then((confirmationResult) => {
+				alert("OTP Sent Successfully!");
+				window.confirmationResult = confirmationResult;
+				setshow(true);
+			})
+			.catch((err) => {
+				alert(err);
+				window.location.reload();
+			});
+	};
 
-    if(OTP.length === 6){
+	const handleSubmit = (event) => {
+		if (OTP === "") {
+			return;
+		}
+		event.preventDefault();
 
-      let confirmationResult = window.confirmationResult
+		if (OTP.length === 6) {
+			let confirmationResult = window.confirmationResult;
 
-      confirmationResult
-      .confirm(OTP)
-      .then((result) => {
-        setUser(result.user)
-        alert(
-          "Voter verified successfully"
-        )
+			confirmationResult
+				.confirm(OTP)
+				.then((result) => {
+					setUser(result.user);
+					alert("Voter verified successfully");
 
-        if (loadVoterAccount({voterName},{voterID},{phoneNumber})){
-          alert(
-            "Voter Login Successful"
-          )
-          fetchVoterLogin(collectionID)
-          routeChange()
-        } 
-        else {
-          alert(
-            "Voter Login Failed"
-          )
-        }
+					if (
+						loadVoterAccount(
+							{ voterName },
+							{ voterID },
+							{ phoneNumber },
+						)
+					) {
+						alert("Voter Login Successful");
+						fetchVoterLogin(collectionID);
+						routeChange();
+					} else {
+						alert("Voter Login Failed");
+					}
+				})
+				.catch((error) => {
+					alert("User not verified please retry again");
+					window.location.reload(false);
+				});
+		}
+	};
 
-      })
-      .catch((error) => {
-        alert(
-          "User not verified please retry again"
-        )
-        window.location.reload(false)
-      })
-
-    }
-  }
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h2" variant="h5">
-            Voter Sign in
-          </Typography>
-          <Box component="form" validate sx={{ mt: 1 }}>
-            {/* Voter Name */}
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              label="Full Name as per Voter ID"
-              name="votername"
-              value = {voterName}
-              onChange= {(e) => setvoterName(e.target.value)}
-            />
-            {/* Voter ID */}
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              label="Voter ID"
-              name="voterid"
-              value={voterID}
-              onChange={ (e) => {setvoterID(e.target.value) }}
-            />
-            {/* Phone Number */}
-            <TextField 
-              margin="normal"
-              required
-              fullWidth
-              label="Phone Number with Country Code"
-              name="phoneNumber"
-              value = {phoneNumber}
-              inputProps={{
-              inputmode: 'tel', 
-              pattern: '[0-9]*', }}
-              onChange={(e) => { setphoneNumber(e.target.value) }}
-            />
-            {/* Generate OTP Button */}
-            <Button 
-            onClick={sendOTP}
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            style={{ display: !show ? "block" : "none" }}>
-              Send OTP
-            </Button>
-            {/* CAPTCHA to generate OTP */}
-            <div id="recaptcha-container" style={{ display: !show ? "block" : "none" }}></div>
-            <Box component="form" validate sx={{ mt: 1 }}>
-              {/* OTP */}
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                label="OTP"
-                name="OTP"
-                value = {OTP}
-                onChange={(e) => { setOTP(e.target.value) }}
-              />
-              {/* Verify Voter Button */}
-              <Button
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={handleSubmit}
-              type="submit">
-                Verify
-              </Button>
-            </Box>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
-    
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component='main' maxWidth='xs'>
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component='h2' variant='h5'>
+						Voter Sign in
+					</Typography>
+					<Box component='form' validate sx={{ mt: 1 }}>
+						{/* Voter Name */}
+						<TextField
+							margin='normal'
+							required
+							fullWidth
+							label='Full Name as per Voter ID'
+							name='votername'
+							value={voterName}
+							onChange={(e) => setvoterName(e.target.value)}
+						/>
+						{/* Voter ID */}
+						<TextField
+							margin='normal'
+							required
+							fullWidth
+							label='Voter ID'
+							name='voterid'
+							value={voterID}
+							onChange={(e) => {
+								setvoterID(e.target.value);
+							}}
+						/>
+						{/* Phone Number */}
+						<TextField
+							margin='normal'
+							required
+							fullWidth
+							label='Phone Number with Country Code'
+							name='phoneNumber'
+							value={phoneNumber}
+							inputProps={{
+								inputmode: "tel",
+								pattern: "[0-9]*",
+							}}
+							onChange={(e) => {
+								setphoneNumber(e.target.value);
+							}}
+						/>
+						{/* Generate OTP Button */}
+						<Button
+							onClick={sendOTP}
+							type='submit'
+							fullWidth
+							variant='contained'
+							sx={{ mt: 3, mb: 2 }}
+							style={{ display: !show ? "block" : "none" }}>
+							Send OTP
+						</Button>
+						{/* CAPTCHA to generate OTP */}
+						<div
+							id='recaptcha-container'
+							style={{ display: !show ? "block" : "none" }}></div>
+						<Box component='form' validate sx={{ mt: 1 }}>
+							{/* OTP */}
+							<TextField
+								margin='normal'
+								required
+								fullWidth
+								label='OTP'
+								name='OTP'
+								value={OTP}
+								onChange={(e) => {
+									setOTP(e.target.value);
+								}}
+							/>
+							{/* Verify Voter Button */}
+							<Button
+								fullWidth
+								variant='contained'
+								sx={{ mt: 3, mb: 2 }}
+								onClick={handleSubmit}
+								type='submit'>
+								Verify
+							</Button>
+						</Box>
+					</Box>
+				</Box>
+				<Copyright sx={{ mt: 8, mb: 4 }} />
+			</Container>
+		</ThemeProvider>
+	);
 }
--- src/components/pages/Voting.js
+++ src/components/pages/Voting.js
@@ -1,212 +1,237 @@
-import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
-import { vote } from '../web3/Web3'
-import { useNavigate } from 'react-router-dom'
-import { Grid , Paper ,Avatar } from '@material-ui/core'
-import Typography from '@mui/material/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import { db, doc, getDoc, query, where, collection, getDocs } from "../../firebase/config";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import { vote } from "../web3/Web3";
+import { useNavigate } from "react-router-dom";
+import { Grid, Paper, Avatar } from "@material-ui/core";
+import Typography from "@mui/material/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import {
+	db,
+	doc,
+	getDoc,
+	query,
+	where,
+	collection,
+	getDocs,
+} from "../../firebase/config";
 // import { candidate1 , candidate2, candidate3 } from '../images/Voting';
 
-let voterCollectionID = '';
+let voterCollectionID = "";
 export const fetchVoterLogin = (VoterID) => {
-  
-  if(VoterID === "HTU6548521") {
-    voterCollectionID = "432yNM9WwVRhYOg10PlY";
-  }else if(VoterID === "KLE9852145"){
-    voterCollectionID = "EdgWaYGGSdupIKxleYSe";
-  }else if(VoterID === "SEW6521432"){
-    voterCollectionID = "Ouv1YADgMobbNO1Gxxx3";
-  }else if(VoterID === "DGU8524763"){
-    voterCollectionID = "urqEOntG6IlkBccG53YN";
-  }
-
-}
-
+	if (VoterID === "HTU6548521") {
+		voterCollectionID = "432yNM9WwVRhYOg10PlY";
+	} else if (VoterID === "KLE9852145") {
+		voterCollectionID = "EdgWaYGGSdupIKxleYSe";
+	} else if (VoterID === "SEW6521432") {
+		voterCollectionID = "Ouv1YADgMobbNO1Gxxx3";
+	} else if (VoterID === "DGU8524763") {
+		voterCollectionID = "urqEOntG6IlkBccG53YN";
+	}
+};
 
 const Voting = () => {
+	let navigate = useNavigate();
 
-  let navigate = useNavigate()
+	//voter details
+	const [name, setName] = useState("");
+	const [constituency, setConstituency] = useState("");
+	const [state, setState] = useState("");
 
-  //voter details
-  const [name, setName] = useState("");
-  const [constituency, setConstituency] = useState("");
-  const [state, setState] = useState("");
+	//candidate details
+	const [candidateDetails, setCandidateDetails] = useState([]);
+	const [party, setParty] = useState([]);
+	const [candidateID, setCandidateID] = useState(0);
+	const [voterIndex, setvoterIndex] = useState(0);
 
-  //candidate details
-  const [candidateDetails, setCandidateDetails] = useState([]);
-  const [party, setParty] = useState([])
-  const [candidateID, setCandidateID] = useState(0)
-  const [voterIndex, setvoterIndex] = useState(0)
+	//fetching voter details
+	const fetchVoterData = async () => {
+		const docRef = doc(db, "voter-details", "432yNM9WwVRhYOg10PlY");
+		const docSnap = await getDoc(docRef);
 
-  //fetching voter details
-  const fetchVoterData = async () => {
-    const docRef = doc(db, "voter-details", "432yNM9WwVRhYOg10PlY");
-    const docSnap = await getDoc(docRef);
-  
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-    }
-    setName(docSnap.data().Name);
-    setConstituency(docSnap.data().Constituency);
-    setState(docSnap.data().State);
+		if (docSnap.exists()) {
+			console.log("Document data:", docSnap.data());
+		} else {
+			// doc.data() will be undefined in this case
+			console.log("No such document!");
+		}
+		setName(docSnap.data().Name);
+		setConstituency(docSnap.data().Constituency);
+		setState(docSnap.data().State);
 
-    console.log(name, constituency, state);
-  }
-  fetchVoterData();
+		console.log(name, constituency, state);
+	};
+	fetchVoterData();
 
-  //fetching candidate details
-  const fetchCandidateData = async () => {
-    const q = query(collection(db, "candidate-details"), where("wardnum", "==", "55"));
+	//fetching candidate details
+	const fetchCandidateData = async () => {
+		const q = query(
+			collection(db, "candidate-details"),
+			where("wardnum", "==", "55"),
+		);
 
-    const querySnapshot = await getDocs(q);
-    
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-    });
+		const querySnapshot = await getDocs(q);
 
-    setCandidateDetails(querySnapshot);
-  }
+		querySnapshot.forEach((doc) => {
+			// doc.data() is never undefined for query doc snapshots
+			console.log(doc.id, " => ", doc.data());
+		});
 
-  fetchCandidateData();
+		setCandidateDetails(querySnapshot);
+	};
 
-  candidateDetails.forEach((candidate) => {
-    console.log(candidate.data().firstname, candidate.data().lastname, " - ", candidate.data().partyname)
-  });
+	fetchCandidateData();
 
-    const castVote = (event, _candidateID) => {
+	candidateDetails.forEach((candidate) => {
+		console.log(
+			candidate.data().firstname,
+			candidate.data().lastname,
+			" - ",
+			candidate.data().partyname,
+		);
+	});
 
-        event.preventDefault()
-        setvoterIndex((voterIndex) => voterIndex + 1)
-        setCandidateID(_candidateID)
-        (candidateID === 1) ? setParty('REP') : 
-        (candidateID === 2) ? setParty('DEM') :
-        (candidateID === 3) ? setParty('KW') :
-        alert(
-            "There is an error"
-        )
+	const castVote = (event, _candidateID) => {
+		event.preventDefault();
+		setvoterIndex((voterIndex) => voterIndex + 1);
+		setCandidateID(_candidateID)(candidateID === 1)
+			? setParty("REP")
+			: candidateID === 2
+			? setParty("DEM")
+			: candidateID === 3
+			? setParty("KW")
+			: alert("There is an error");
 
-        window.confirm(
-            "Press 'OK' to vote for Candidate" + {candidateID} + "of" + {party} + "?"
-            )
+		window.confirm(
+			"Press 'OK' to vote for Candidate" +
+				{ candidateID } +
+				"of" +
+				{ party } +
+				"?",
+		);
 
-        try{
-            vote(voterIndex, candidateID)
-            alert(
-                "Your vote has successfully been cast"
-            )
-            navigate(`/`)
-        }catch(error){
-            alert(
-                " There was an error " + {error}
-            )
-        }
+		try {
+			vote(voterIndex, candidateID);
+			alert("Your vote has successfully been cast");
+			navigate(`/`);
+		} catch (error) {
+			alert(" There was an error " + { error });
+		}
+	};
 
-    }
+	const paperstyle = {
+		padding: 30,
+		height: "50vh",
+		width: "40vh",
+		margin: "60px auto",
+	};
+	const votepaperstyle = {
+		padding: 30,
+		height: "60vh",
+		width: "120vh",
+		margin: "40px auto",
+	};
+	const useStyles = makeStyles((theme) => ({
+		paper: {
+			padding: theme.spacing(1),
+			textAlign: "center",
+			color: theme.palette.text.secondary,
+			margin: "80px auto",
+			maxHeight: 200,
+			maxWidth: 900,
+		},
+		image: {
+			width: 128,
+			height: 128,
+		},
+		img: {
+			margin: "left",
+			display: "block",
+			maxWidth: "100%",
+			maxHeight: "100%",
+		},
 
-    const paperstyle={padding: 30 , height:'50vh', width:'40vh',margin:"60px auto"}
-    const votepaperstyle={padding: 30 , height:'60vh', width:'120vh',margin:"40px auto"}
-    const useStyles = makeStyles((theme) => ({
-     
-      paper: {
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-        margin:"80px auto",
-        maxHeight:200,
-        maxWidth: 900,
-      },
-      image: {
-        width: 128,
-        height: 128,
-      },
-      img: {
-        margin: 'left',
-        display: 'block',
-        maxWidth: '100%',
-        maxHeight: '100%',
-      },  
-   
-      inline: {
-        display: 'inline',
-      },
-    }));
+		inline: {
+			display: "inline",
+		},
+	}));
 
-    return (
-      <Grid container /*spacing={2}*/ style={{backgroundColor: 'rgb(255, 194, 0, 25%)', height: '89vh'}}>  
-      {/* Displaying the voter details */}
-            <Paper elevation={10} style={paperstyle} >
-              <Grid align='center'>
-                <Avatar src="/broken-image.jpg" />
-                <Typography component="h2" variant="h5">
-                  Voter Details
-                </Typography>
-                  <Grid align='left'>
-                  < br />
-                  < br />
-                  <Typography variant="body1">
-                    <b>Name: </b>{name}
-                  </Typography>
-                  < br />
-                  <Typography variant="body1">
-                  <b>Constituency: </b>{constituency}
-                  </Typography>
-                  < br />
-                  <Typography variant="body1">
-                    <b>State: </b>{state}
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Paper>
+	return (
+		<Grid
+			container
+			/*spacing={2}*/ style={{
+				backgroundColor: "rgb(255, 194, 0, 25%)",
+				height: "89vh",
+			}}>
+			{/* Displaying the voter details */}
+			<Paper elevation={10} style={paperstyle}>
+				<Grid align='center'>
+					<Avatar src='/broken-image.jpg' />
+					<Typography component='h2' variant='h5'>
+						Voter Details
+					</Typography>
+					<Grid align='left'>
+						<br />
+						<br />
+						<Typography variant='body1'>
+							<b>Name: </b>
+							{name}
+						</Typography>
+						<br />
+						<Typography variant='body1'>
+							<b>Constituency: </b>
+							{constituency}
+						</Typography>
+						<br />
+						<Typography variant='body1'>
+							<b>State: </b>
+							{state}
+						</Typography>
+					</Grid>
+				</Grid>
+			</Paper>
 
-            {/* Displaying the candidate details */}
-            <Paper elevation={10} style={votepaperstyle} >
-            <List>
-              <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                  <Avatar alt="partyname" src="/static/images/avatar/1.jpg" />
-                </ListItemAvatar>
-                <ListItemText
-                  primary="BJP"
-                  secondary={
-                    <React.Fragment>
-                      <Typography
-                        component="span"
-                        variant="body2"
-                        
-                        color="textPrimary"
-                      >
-                        
-                      </Typography>
-                      {"CANDIDATE1"}
-                    </React.Fragment>
-                  }
-                />
-                <Button style={{
-                  backgroundColor: 'rgb(255, 194, 0)'
-                }}
-                onClick={(e) => {
-                    castVote(e, 3)
-                }}>
-                Vote
-              </Button>
-                </ListItem>
-                <Divider variant="inset" component="li" />
-                
-            </List>
-            </Paper> 
-          </Grid>
-    )
-  
-}
+			{/* Displaying the candidate details */}
+			<Paper elevation={10} style={votepaperstyle}>
+				<List>
+					<ListItem alignItems='flex-start'>
+						<ListItemAvatar>
+							<Avatar
+								alt='partyname'
+								src='/static/images/avatar/1.jpg'
+							/>
+						</ListItemAvatar>
+						<ListItemText
+							primary='BJP'
+							secondary={
+								<React.Fragment>
+									<Typography
+										component='span'
+										variant='body2'
+										color='textPrimary'></Typography>
+									{"CANDIDATE1"}
+								</React.Fragment>
+							}
+						/>
+						<Button
+							style={{
+								backgroundColor: "rgb(255, 194, 0)",
+							}}
+							onClick={(e) => {
+								castVote(e, 3);
+							}}>
+							Vote
+						</Button>
+					</ListItem>
+					<Divider variant='inset' component='li' />
+				</List>
+			</Paper>
+		</Grid>
+	);
+};
 
 export default Voting;
